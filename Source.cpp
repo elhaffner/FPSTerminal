de0a763e71c410f1966b14f37987a724fb65306d
Lucas Haffner <lucashaffner2001@gmail.com>
2025-08-10 22:41:19 +0100

    initial

---
+++ Source.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <Windows.h>
+using namespace std;
+
+int nScreenWidth = 120;
+int nScreenHeight = 40;
+
+float fPlayerX = 0.0f;
+float fPlayerY = 0.0f;
+float fPlayerA = 0.0f;
+
+int nMapHeight = 16;
+int nMapWidth = 16;
+
+
+int main() {
+    wchar_t *screen = new wchar_t[nScreenWidth*nScreenHeight];
+    HANDLE hConsole = CreateConsoleScreenBuffer(GENERIC_READ | GENERIC_WRITE, 0, NULL, CONSOLE_TEXTMODE_BUFFER, NULL);
+    SetConsoleActiveScreenBuffer(hConsole);
+    DWORD dwBytesWritten = 0;
+
+    while(1) 
+    {
+
+
+
+        screen[nScreenWidth*nScreenHeight-1] = '\0';
+        WriteConsoleOutputCharacter(hConsole, screen, nScreenWidth*nScreenHeight, {0, 0}, &dwBytesWritten);
+    }
+
+    
+
+    
+
+    return 0;
+}

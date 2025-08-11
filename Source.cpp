6f067aa0513a1947fd205b2e6662d3deabbc23dc
Lucas Haffner <lucashaffner2001@gmail.com>
2025-08-11 07:47:19 +0100

    Message

--- Source.cpp
+++ Source.cpp
@@ -12,6 +12,11 @@ float fPlayerA = 0.0f;
 int nMapHeight = 16;
 int nMapWidth = 16;
 
+float fFOV = 3.14159 / 4.0;
+float fDepth = 16.0f;
+
+
+
 
 int main() {
     wchar_t *screen = new wchar_t[nScreenWidth*nScreenHeight];
@@ -19,10 +24,55 @@ int main() {
     SetConsoleActiveScreenBuffer(hConsole);
     DWORD dwBytesWritten = 0;
 
+    wstring map;
+
+    map +=L"################";
+    map +=L"#..............#";
+    map +=L"#..............#";
+    map +=L"#..............#";
+    map +=L"#..............#";
+    map +=L"#..............#";
+    map +=L"#..............#";
+    map +=L"#..............#";
+    map +=L"#..............#";
+    map +=L"#..............#";
+    map +=L"#..............#";
+    map +=L"#..............#";
+    map +=L"#..............#";
+    map +=L"#..............#";
+    map +=L"#..............#";
+    map +=L"################";
+    
+
     while(1) 
     {
 
+        for(int x = 0; x < nScreenWidth; x++) {
+            float fRayAngle = (fPlayerA - fFOV / 2.0f) + ((float)x / float(nScreenWidth)) * fFOV;
 
+            float fDistanceToWall = 0;
+            bool bHitWall = false;
+
+            float fEyeX = sinf(fRayAngle);
+            float fEyeY = cosf(fRayAngle);
+
+            while(!bHitWall && fDistanceToWall < fDepth) {
+                fDistanceToWall += 01.f;
+
+                int nTestX = (int)(fPlayerX+fEyeX*fDistanceToWall);
+                int nTestY = (int)(fPlayerY+fEyeY*fDistanceToWall);
+            }
+
+            if (nTestX < 0 || nTestX >= nMapWidth || nTestY < 0 || nTestY >= nMapHeight){
+                bHitWall = true;
+                fDistanceToWall = fDepth;
+            } else {
+                if (map[nTestY * nMapWidth + nTestX] == '#') {
+                    bHitWall = true;
+                }
+            }
+
+        }
 
         screen[nScreenWidth*nScreenHeight-1] = '\0';
         WriteConsoleOutputCharacter(hConsole, screen, nScreenWidth*nScreenHeight, {0, 0}, &dwBytesWritten);
